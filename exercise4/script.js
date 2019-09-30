e52d147c8f45b5e287ca4aefb0f3da61120908d2
Hazel Thexton <hazelthexton@videotron.ca>
2019-09-30 11:22:03 -0400

    EX4: Added a rating box

--- exercise4/script.js
+++ exercise4/script.js
@@ -1,6 +1,29 @@
+// sayHello()
+//
+//
 function sayHello() {
   var nameFirst = document.getElementById("nameFirst").value;
   var nameLast = document.getElementById("nameLast").value;
 
   document.getElementById("hello").innerHTML= "Hello "+ nameFirst + " " + nameLast + "!";
+}
+
+// rate()
+//
+//
+function rate() {
+  var rating = document.getElementById("rating").value;
+
+  if (rating>10){
+    document.getElementById("response").innerHTML= rating + "???? It's off the charts!!!";
+  }
+  else if (rating>7){
+    document.getElementById("response").innerHTML= rating + "? Awesome! I'm glad.";
+  }
+  else if (rating>4){
+    document.getElementById("response").innerHTML= rating + "? Well, that's okay, but not great.";
+  }
+  else {
+    document.getElementById("response").innerHTML= rating + "? That sucks, sorry to hear that.";
+  }
 }
